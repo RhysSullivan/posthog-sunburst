7f27d6ee367470c1174ce6fad38f334afdcb2a13
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-20 20:01:05 -0700

    tanstack query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,8 @@ import { MyResponsiveSunburst } from "./components";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="w-[800px] h-[800px] text-black">
-          <MyResponsiveSunburst />
-        </div>
+      <div className="w-[800px] h-[800px] text-black">
+        <MyResponsiveSunburst />
       </div>
     </main>
   );
