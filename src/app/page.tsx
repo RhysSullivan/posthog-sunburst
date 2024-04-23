7b25cd12bea000ffa392af9c95d05b53266c171b
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-04-23 01:12:28 -0700

    github icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,15 @@ import {
   QueryProvider,
   Sidebar,
 } from "./components";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function Home() {
   const [query, setQuery] = useState<null | QueryContext>(null);
   const [chartOptions, setChartOptions] = useState<null | ChartOptions>(null);
   return (
     <QueryProvider value={{ query, setQuery, setChartOptions, chartOptions }}>
-      <main className="flex min-h-screen flex-row">
+      <main className="flex min-h-screen flex-row relative">
         <Sidebar />
         <div className="flex-grow flex-col flex justify-center items-center">
           <div className="w-[800px] h-[800px] text-black">
@@ -22,6 +24,9 @@ export default function Home() {
           </div>
           <BottomBar />
         </div>
+        <Link href="https://github.com/RhysSullivan/posthog-sunburst/">
+          <GitHubLogoIcon className="absolute top-4 right-4 h-8 w-8" />
+        </Link>
       </main>
     </QueryProvider>
   );
